74a274f90966333187490dd413b79d475ae5e6e8
Etty Sekhon <sekhoe01@118-UX.local>
2017-03-24 13:22:56 +0000

    mocha test

--- basic/index.js
+++ basic/index.js
@@ -1,11 +1,20 @@
-const Nightmare = require('nightmare');
-nightmare = new Nightmare();
+const Nightmare = require('nightmare')
+const assert = require('assert')
 
-nightmare.goto('https://www.google.co.uk/')
-  .end()
-  .then((result) => {
-    console.log('success');
+describe('Load a Page', function() {
+  this.timeout('30s')
+  let nightmare = null
+
+  beforeEach(() => {
+    nightmare = new Nightmare()
+  });
+
+  describe('/ (Home Page)', () => {
+    it('should load without error', done => {
+      nightmare.goto('https://www.google.co.uk/')
+        .end()
+        .then(function (result) { done() })
+        .catch(done)
+    })
   })
-  .catch((err) => {
-    console.log('fail', err);
-  })
+});
